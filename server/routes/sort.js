294c02667b81ad0336d18f66f1f54b426e310a60
Himanshu Pant <himanshu.pn3@gmail.com>
2018-01-26 10:17:23 -0800

    removed multer

--- server/routes/sort.js
+++ server/routes/sort.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const multer = require("multer");
 const { strSorter } = require("../actions/sortCtrl");
 
-const upload = multer();
 const router = express.Router();
 router.post("/", strSorter);
 
